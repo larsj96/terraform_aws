3c9189db262879acf5ed6a470779b23699b9ebcc
LARS-JØRGEN NILSENgit config --global user.email ljn@aztek.no <ljn@aztek.no>
2022-11-30 14:14:55 +0100

    changed AMI to ubuntu + added cloudinit

--- Test-servers.tf
+++ Test-servers.tf
@@ -1,8 +1,8 @@
 ## ================================  Node Instances ================================
 resource "aws_instance" "test-nodes" {
  # ami             = var.aws_ami_id #"ami-0cd31be676780afa7"
-  ami = "ami-02aeff1a953c5c2ff"
-  instance_type   = "t2.micro"
+  ami = "ami-00b696228b0185ffe"
+  instance_type   = "t3.micro"
   key_name        = aws_key_pair.ec2loginkey.key_name
   security_groups = ["ssh"]
   user_data       = file("user-data-ansible-nodes.sh")
